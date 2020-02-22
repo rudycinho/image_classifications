105a3ec51dd512527f8e1c8abfa983a0e793f0e8
r.martinez1 <r.martinez@empresasim.com>
2020-02-22 12:10:53 -0400

    classification funtion

--- web/app.py
+++ web/app.py
@@ -38,3 +38,77 @@ class Register(Resource):
             "password":hashed_pw,
             "tokens":4
         })
+
+        ret_json = {
+            "status":200,
+            "msg":"You successfully signed up for this API"
+        }
+
+def generate_return_dictionary(status,msg):
+    ret_json = {
+        "status":staticmethod,
+        "msg":msg
+    }
+    return ret_json
+
+def verify_pw(username, password):
+    if not user_exists(username):
+        return False
+    hashed_pw = users.find({
+        "username":username
+    })[0]["Password"]
+
+    return bcrypt.hashedpw(password.encode('utf8'),hashed_pw)==hashed_pw
+
+def verify_credentials(username, password):
+    if not user_exists(username):
+        return generate_return_dictionary(301,"Invalid Username"),True
+    correct_pw = verify_pw(username,password)
+    if not correct_pw:
+        return generate_return_dictionary(302,"Invalid Password"),True
+    return None,False
+
+class Classify(Resource):
+    def post(self):
+        posted_data = request.get_json()
+
+        username = posted_data["username"]
+        password = posted_data["password"]
+        url      = posted_data["url"]
+
+        #TODO
+        ret_json, error = verify_credentials(username,password)
+        if error:
+            return jsonify(ret_json)
+
+        tokens = users.find({
+            "username":username
+        })[0]["tokens"]
+
+        #TODO
+        if tokens<=0:
+            return jsonify(generate_return_dictionary(303,"Not enough tokens!"))
+
+        r = requests.get(url)
+        ret_json = {}
+
+        with open("temp.jpg","wb") as f:
+            f.write(r.content)
+            proc = subprocess.Popen('python classify_image.py --model_dir=. --image_file=./temp.jpg')
+            proc.communicate()[0]
+            proc.wait()
+            with open('text.txt') as g:
+                ret_json = json.load(g)
+
+        users.update({
+            "username":username
+        },{
+            "$set":{
+                "tokens":tokens-1
+            }
+        })
+        return ret_json
+
+
+
+
